18fa8891dc27bfdc5273b6359f3db2e1b32d59a6
gilad eini <giladeini@gmail.com>
2016-02-02 19:46:48 +0200

    clean up1

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -21,61 +21,45 @@ import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import constants.DBConstants;
 
 
-/**
- * Servlet implementation class LoginServlet
- */
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
+    public LoginServlet()
+    {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub	
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("kabab1");
-		
-		
-		try{
-		Context context = new InitialContext();
-		BasicDataSource ds = (BasicDataSource)context.lookup(DBConstants.DB_DATASOURCE);
-		Connection conn = ds.getConnection();
-		PreparedStatement pstmt = conn.prepareStatement(DBConstants.INSERT_USER_STMT);
-		
-		pstmt.setString(1,request.getParameter("username"));
-		pstmt.setString(2,request.getParameter("password"));
-		pstmt.executeUpdate();
-		
-		//commit update
-		conn.commit();
-		//close statements
-		System.out.println("kabab2");
-		pstmt.close();
-		//close connection
-		conn.close();
-		
-	} catch (SQLException | NamingException e) {
-		getServletContext().log("Error while closing connection", e);
-		response.sendError(500);//internal server error
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		System.out.println("inside LoginServlet doPost");
+		try
+		{
+			Context context = new InitialContext();
+			BasicDataSource ds = (BasicDataSource)context.lookup(DBConstants.DB_DATASOURCE);
+			Connection conn = ds.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(DBConstants.INSERT_USER_STMT);
+			
+			pstmt.setString(1,request.getParameter("username"));
+			pstmt.setString(2,request.getParameter("password"));
+			pstmt.executeUpdate();
+			
+			conn.commit();
+			pstmt.close();
+			conn.close();
+		}
+		catch (SQLException | NamingException e) 
+		{
+			getServletContext().log("Error while closing connection", e);
+			response.sendError(500);//internal server error
+		}
+		response.sendRedirect("index.html");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	response.sendRedirect("index.html");
-	response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
 }
