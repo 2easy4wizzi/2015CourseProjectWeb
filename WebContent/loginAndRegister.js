8c0fa9dcd61aa878233789f9056b2dc81021aeec
gilad eini <giladeini@gmail.com>
2016-02-02 20:26:32 +0200

    login and register js

---
+++ WebContent/loginAndRegister.js
@@ -0,0 +1,30 @@
+
+
+angular.module('loginAndRegister', []).controller('AdminCtrl', ['$scope', '$http',
+	function ($scope, $http) {
+	
+	        $scope.Login=function () {
+	     debugger;
+	     //Defining the $http service for login the admin user
+	     $http({
+	         method: 'POST',
+	         url: 'http://localhost:8080/webGilad/LoginServlet',
+	         params: { username: $scope.U_Name, password: $scope.U_PWD },
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	
+	     }).success(function (result) {
+	
+	         if (result == true) {
+	             alert('user is valid');
+	         }
+	         else {
+	             alert('unauthorised access!');
+	         }
+	     })
+	         .error(function (error) {
+	             //Showing error message 
+	             $scope.status = 'Unable to connect' + error.message;
+	         });
+	 }
+	   // $scope.Login();
+	    }]);
